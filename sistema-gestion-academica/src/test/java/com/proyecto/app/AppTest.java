f71ce6ad64cdd07cfb33b883eaeef571b1e38590
cpinopacheco <cpinopacheco64@gmail.com>
2026-05-13 12:55:22 -0400

    Configuracion inicial Maven y pruebas automatizadas

---
+++ sistema-gestion-academica/src/test/java/com/proyecto/app/AppTest.java
@@ -0,0 +1,12 @@
+package com.proyecto.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void pruebaInicial() {
+        assertTrue(true);
+    }
+}
