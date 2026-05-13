835f3557a07d4d356164fb46a6f5b84281c2a166
cpinopacheco <cpinopacheco64@gmail.com>
2026-05-13 14:34:34 -0400

    Mejora de logica de asistencia y pruebas unitarias

--- sistema-gestion-academica/src/test/java/com/proyecto/app/AppTest.java
+++ sistema-gestion-academica/src/test/java/com/proyecto/app/AppTest.java
@@ -1,12 +1,29 @@
 package com.proyecto.app;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
 
     @Test
-    void pruebaInicial() {
-        assertTrue(true);
+    void validarAsistenciaAprobada() {
+
+        App app = new App();
+
+        boolean resultado = app.validarAsistencia(80);
+
+        assertTrue(resultado);
+    }
+
+    @Test
+    void validarAsistenciaReprobada() {
+
+        App app = new App();
+
+        boolean resultado = app.validarAsistencia(60);
+
+        assertFalse(resultado);
     }
 }
